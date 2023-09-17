5799701f74a6fc1e42439be24f33df331819cd27
xathospap <xanthos.papanikolaou@gmail.com>
2023-09-17 13:34:33 +0300

    adding test workflow

--- run_test_suite.py
+++ run_test_suite.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-import os, sys, subprocess, argparse, ftplib
+import os
+import sys
+import subprocess
+import argparse
+import ftplib
 
 ds1 = """DIOA  A 12602S011
 DIOB  A 12602S012
@@ -20,30 +24,75 @@ DIOA 2005-12-15 00:00:00.000000000 2006-05-16 23:59:59.000000000 Transmission st
 DIOA 2005-12-15 00:00:00.000000000 2006-05-16 23:59:59.000000000 Transmission stopped
 """
 
-progs = [
-  {'name': 'test-sinex', 'args': ['DATA_DIR/foobar'], 'exit': 1},
-  {'name': 'test-sinex', 'args': ['DATA_DIR/dpod2020.snx'], 'exit': 0},
-  {'name': 'test-site-id-ndomes', 'args': ['DATA_DIR/dpod2020.snx', 'DIOA', 'DIOB', 'FOO', 'BAR', 'DIOC'], 'sout': ds1, 'exit': 0},
-  {'name': 'test-site-id-wdomes', 'args': ['DATA_DIR/dpod2020.snx', 'DIOA', '12602S011', 'A', 'B', 'DIOB', '12602S012', 'FOO', 'BAR', 'BAR', '12602S011', 'DIOC', '123'], 'sout': ds2, 'exit': 0},
-  {'name': 'test-site-eccentricity', 'args': ['DATA_DIR/dpod2020.snx', 'FOO', 'DIOA', 'BAR1', 'DIOB', 'DIOC'], 'sout': ds3, 'exit': 0},
-  {'name': 'test-parameter-exists', 'args': [], 'exit': 0},
-  {'name': 'test-data-reject', 'args': ['DATA_DIR/dpod2020.snx', 'FOO', 'DIOA', 'BAR1', 'DIOB', 'DIOC'], 'sout': ds4, 'exit': 0}
-  ]
+progs = [{'name': 'test-sinex',
+          'args': ['DATA_DIR/foobar'],
+          'exit': 1},
+         {'name': 'test-sinex',
+          'args': ['DATA_DIR/dpod2020.snx'],
+          'exit': 0},
+         {'name': 'test-site-id-ndomes',
+          'args': ['DATA_DIR/dpod2020.snx',
+                   'DIOA',
+                   'DIOB',
+                   'FOO',
+                   'BAR',
+                   'DIOC'],
+          'sout': ds1,
+          'exit': 0},
+         {'name': 'test-site-id-wdomes',
+          'args': ['DATA_DIR/dpod2020.snx',
+                   'DIOA',
+                   '12602S011',
+                   'A',
+                   'B',
+                   'DIOB',
+                   '12602S012',
+                   'FOO',
+                   'BAR',
+                   'BAR',
+                   '12602S011',
+                   'DIOC',
+                   '123'],
+          'sout': ds2,
+          'exit': 0},
+         {'name': 'test-site-eccentricity',
+          'args': ['DATA_DIR/dpod2020.snx',
+                   'FOO',
+                   'DIOA',
+                   'BAR1',
+                   'DIOB',
+                   'DIOC'],
+          'sout': ds3,
+          'exit': 0},
+         {'name': 'test-parameter-exists',
+          'args': [],
+          'exit': 0},
+         {'name': 'test-data-reject',
+          'args': ['DATA_DIR/dpod2020.snx',
+                   'FOO',
+                   'DIOA',
+                   'BAR1',
+                   'DIOB',
+                   'DIOC'],
+          'sout': ds4,
+          'exit': 0}]
+
 
 def check_file_vs_str(file, str):
-  with open(file, 'r') as fin:
-    ## read whole file to a string
-    fstr = fin.read()
-    return (fstr == str)
+    with open(file, 'r') as fin:
+        # read whole file to a string
+        fstr = fin.read()
+        return (fstr == str)
+
 
 class myFormatter(argparse.ArgumentDefaultsHelpFormatter,
                   argparse.RawTextHelpFormatter):
     pass
 
+
 parser = argparse.ArgumentParser(
     formatter_class=myFormatter,
-    description=
-    'Run validatation programs against SOFA library',
+    description='Run validatation programs against SOFA library',
     epilog=('''National Technical University of Athens,
     Dionysos Satellite Observatory\n
     Send bug reports to:
@@ -73,50 +122,59 @@ parser.add_argument(
     help='Verbose mode on')
 
 if __name__ == '__main__':
-  ## parse cmd
-  args = parser.parse_args()
+# parse cmd
+    args = parser.parse_args()
 
-  verboseprint = print if args.verbose else lambda *a, **k: None
+# verbose print
+    verboseprint = print if args.verbose else lambda *a, **k: None
 
-## Download the latest dpod2020 SINEX file and store it as DATA_DIR/dpod2020.snx
-  if not os.path.isfile(os.path.join(args.data_dir, 'dpod2020.snx')):
-    verboseprint('Downloading latest dpod2020 SINEX file from doris.ign.fr')
-    ftp = ftplib.FTP('doris.ign.fr')
-    ftp.login()
-    ftp.cwd('pub/doris/products/dpod/dpod2020')
-    ftp.retrbinary("RETR {}".format('dpod2020_current.snx.Z'), open(os.path.join(args.data_dir, 'dpod2020.snx.Z'), 'wb').write)
-    ftp.quit()
-## uncompress
-    subprocess.run(['uncompress', '{}'.format(os.path.join(args.data_dir, 'dpod2020.snx.Z'))], check=True)
+# Download the latest dpod2020 SINEX file and store it as DATA_DIR/dpod2020.snx
+    if not os.path.isfile(os.path.join(args.data_dir, 'dpod2020.snx')):
+        verboseprint(
+            'Downloading latest dpod2020 SINEX file from doris.ign.fr')
+        ftp = ftplib.FTP('doris.ign.fr')
+        ftp.login()
+        ftp.cwd('pub/doris/products/dpod/dpod2020')
+        ftp.retrbinary("RETR {}".format('dpod2020_current.snx.Z'), open(
+            os.path.join(args.data_dir, 'dpod2020.snx.Z'), 'wb').write)
+        ftp.quit()
+# uncompress
+        subprocess.run(['uncompress', '{}'.format(
+            os.path.join(args.data_dir, 'dpod2020.snx.Z'))], check=True)
 
-## temporary file for logging/output
-  temp_fn = '.testemp'
+# temporary file for logging/output
+    temp_fn = '.testemp'
 
-  ## call each of the test programs with siutable args
-  for dct in progs:
-## test-program (executable)
-    exe = os.path.join(args.progs_dir, dct['name']) + '.out'
-## check that the program is where expected
-    if not os.path.isfile(exe):
-      print('ERROR Failed to find executable {:}'.format(exe), file=sys.stderr)
-      sys.exit(1)
-## write output (if needed) here
-    ftmp = open(temp_fn, "w")
-## replace DATA_DIR with the actual data_dir in any of the arguments
-    cmdargs = [ x.replace('DATA_DIR',args.data_dir) for x in dct['args'] ]
-## run the command, catch output and exit code
-    verboseprint('Running command: {:}'.format([exe]+cmdargs))
-    result = subprocess.run([exe]+cmdargs, stdout=ftmp, check=False)
-    ftmp.close()
-## check the return code
-    if result.returncode != dct['exit']:
-      print('ERROR Expected a return code {:} and got {:}; exe {:}'.format(dct['exit'], result.returncode, exe), file=sys.stderr)
-      sys.exit(2)
-    else:
-      verboseprint('\tExit Code OK')
-## check the output against the should-be output
-    if 'sout' in dct:
-      if check_file_vs_str(temp_fn, dct['sout']):
-        verboseprint('\tCommand Output OK')
-      else:
-        print('ERROR Program\'s {:} output different than expected'.format(exe), file=sys.stderr)
+    # call each of the test programs with siutable args
+    for dct in progs:
+        # test-program (executable)
+        exe = os.path.join(args.progs_dir, dct['name']) + '.out'
+# check that the program is where expected
+        if not os.path.isfile(exe):
+            print(
+                'ERROR Failed to find executable {:}'.format(exe),
+                file=sys.stderr)
+            sys.exit(1)
+# write output (if needed) here
+        ftmp = open(temp_fn, "w")
+# replace DATA_DIR with the actual data_dir in any of the arguments
+        cmdargs = [x.replace('DATA_DIR', args.data_dir) for x in dct['args']]
+# run the command, catch output and exit code
+        verboseprint('Running command: {:}'.format([exe] + cmdargs))
+        result = subprocess.run([exe] + cmdargs, stdout=ftmp, check=False)
+        ftmp.close()
+# check the return code
+        if result.returncode != dct['exit']:
+            print('ERROR Expected a return code {:} and got {:}; exe {:}'.format(
+                dct['exit'], result.returncode, exe), file=sys.stderr)
+            sys.exit(2)
+        else:
+            verboseprint('\tExit Code OK')
+# check the output against the should-be output
+        if 'sout' in dct:
+            if check_file_vs_str(temp_fn, dct['sout']):
+                verboseprint('\tCommand Output OK')
+            else:
+                print(
+                    'ERROR Program\'s {:} output different than expected'.format(exe),
+                    file=sys.stderr)
